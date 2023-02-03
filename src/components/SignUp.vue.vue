0f39a6735df9518a73ed7a5d910efe2a7e0c9199
Pranjalisathe <pranjali.21910713@viit.ac.in>
2023-02-03 12:51:02 +0530

    EMployee managment System

---
+++ src/components/SignUp.vue.vue
@@ -0,0 +1,69 @@
+<template>
+<h3>Enter  Details page</h3>
+<div class="register">
+<input type="text" v-model="name" placeholder="Enter Name"/>
+<input type="text" v-model="email" placeholder="Enter Email"/>
+<input type="text" v-model="role" placeholder="Enter Role"/>
+<input type="password" v-model="password" placeholder="Enter password"/>
+
+<button v-on:click="signup">Enter</button>
+</div>
+</template>
+<script>
+import axios from 'axios'
+export default{
+    name:"SignUp",
+    data()
+    {
+        return{
+            name:"",
+            email:"",
+            role:"",
+            password:""
+        }
+    },
+    methods:{
+       async signup()
+        {
+            console.log("Sign up",this.name,this.email,this.role);
+             let result = await axios.post("http://localhost:3000/users",{
+                email:this.email,
+                name:this.name,
+                role:this.role,
+                password:this.password
+             });
+             console.warn(result);
+             if(result.status==200)
+             {
+                alert("Details  entered successfully !")
+             }
+             localStorage.setItem("user-info",JSON.stringify(result.data))
+        }
+    }
+}
+</script>
+<style>
+.register input{
+    width :300px ;
+    height: 40px;
+    padding-left:20px;
+    display:block;
+    margin-bottom:30px;
+    margin-left:auto;
+    margin-right:auto;
+    border:1px solid black;
+
+}
+.register button{
+ width :320px ;
+    height: 40px;
+    padding-left:20px;
+    display:block;
+    margin-bottom:30px;
+    margin-left:auto;
+    margin-right:auto;
+    color:black;
+    background-color:orange;
+    border:1px solid black;
+}
+</style>
